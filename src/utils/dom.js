2afbd2233dc5f6f4908e4337908f0aebd72274c8
Anton Furs <anton.furs.dev@gmail.com>
2025-07-28 20:06:54 +0200

    feat: add base project structure

---
+++ src/utils/dom.js
@@ -0,0 +1,27 @@
+// Create DOM element
+export const createElement = ({
+  tag,
+  className,
+  textContent,
+  attributes = {},
+  dataset = {},
+}) => {
+  const element = document.createElement(tag);
+
+  if (className) element.className = className;
+  if (textContent) element.textContent = textContent;
+
+  if (attributes) {
+    Object.entries(attributes).forEach(([key, value]) => {
+      element.setAttribute(key, value ?? '');
+    });
+  }
+
+  if (dataset) {
+    Object.entries(dataset).forEach(([key, value]) => {
+      element.dataset[key] = value ?? '';
+    });
+  }
+
+  return element;
+};
