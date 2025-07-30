3a4dfd9fa92f2db7ba442971801a48121f4c4568
Anton Furs <anton.furs.dev@gmail.com>
2025-07-30 15:03:49 +0200

    Save local changes before cherry-pick

--- playground/main-alex.js
+++ playground/main-alex.js
@@ -1,13 +1,7 @@
 import '@styles/main.scss';
 import { createElement } from '@utils/dom';
 
-const App = () => {
-  const container = createElement({ tag: 'div', className: 'container' });
-  const title = createElement({ tag: 'p', textContent: 'Alex' });
-  container.appendChild(title);
-
-  return container;
-};
-
 const root = document.querySelector('#root');
-root.append(App());
+const title = createElement({ tag: 'p', textContent: 'Alex' });
+
+root.appendChild(title);
