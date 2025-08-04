a17f6546da84e26c7779488a23d0af3a50a1cfe6
Vera Kolodinskaya <vera.kolodinskaya@gmail.com>
2025-08-04 23:19:20 +0300

    refactor: refactoring code

--- src/components/modal-base.js
+++ src/components/modal-base.js
@@ -2,21 +2,27 @@ import { createElement } from '@utils/dom';
 import { buttonCancel, buttonOk } from '@components/button';
 
 export const createModal = ({ title: title, bodyModal = [], footerModal = [] }) => {
-  const modalWindow = createElement({ tag: 'div', className: `modal` });
-  if (title) {
-    const modalTitle = createElement({ tag: 'p', className: 'modal-title', textContent: title });
-    modalWindow.append(modalTitle);
-  }
-
-  bodyModal.forEach(value => modalWindow.appendChild(value));
+  const modalWindow = createElement({ tag: 'dialog', className: `modal` });
+  
+  const header = createElement({ tag: 'div', className: 'modal-header' });
+  modalWindow.append(header);
+  const titleModal = createElement({ tag: 'p', className: 'modal-header__title', textContent: title });
+  header.append(titleModal);
+  
+  const body = createElement({ tag: 'div', className: 'modal-body' });
+  modalWindow.append(body);
+  bodyModal.forEach(value => body.appendChild(value));
 
   const footerModalBlock = createElement({ tag: 'div', className: 'modal-footer' });
   modalWindow.append(footerModalBlock);
-
   footerModal.forEach(value => footerModalBlock.appendChild(value));
-
   const footerModalButton = createElement({ tag: 'div', className: 'modal-footer__button' });
   footerModalBlock.append(footerModalButton);
   footerModalButton.append(buttonCancel, buttonOk);
+
+  const closeModal = () => {modalWindow.close()}
+  buttonCancel.addEventListener('click', closeModal);
+  
   return modalWindow;
 }
+
