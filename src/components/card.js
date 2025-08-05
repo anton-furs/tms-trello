90bab43b0de0329d4250e839b547f8fafde655f4
Vera Kolodinskaya <vera.kolodinskaya@gmail.com>
2025-08-06 00:03:23 +0300

    refactor: refactoring code

--- src/components/card.js
+++ src/components/card.js
@@ -15,46 +15,40 @@ const formatDate = () => {
 const formatUser = (userName) => {
   const arrayWord = userName.split(' ');
   const firstLetters = arrayWord.map(value => value.charAt(0));
-  return firstLetters.join('');
+  return firstLetters.join('').toUpperCase();
 }
 
 export const createCard = ({ title: title, text: text, user: user, date: date}) => {
   const card = createElement({ tag: 'div', className: 'card' });
-
   const cardContent = createElement({ tag: 'div', className: 'card-content' });
-  card.append(cardContent);
-
   const arrowLeft = createElement({ tag: 'div', className: 'card-content__arrow'});
   const svgArrowLeft = createIcon({ name: 'chevron-left', size: 18, className: 'card-content__arrow-icon', type: 'stroke' });
-  arrowLeft.append(svgArrowLeft);
-  cardContent.append(arrowLeft);
-
   const bodyCard = createElement({ tag: 'div', className: 'card-content__body'});
+  const titleBlock = createElement({ tag: 'div', className: 'card-content__body-block' });
+  const titleCard = createElement({ tag: 'p', className: 'card-content__body-block__title', textContent: title });
+  const basketDelete = createIcon({ name: 'delete-bin', size: 18, className: 'card-content__arrow-icon card-content__body-block__delete', type: 'stroke' });
+  const textCard = createElement({ tag: 'div', className: 'card-content__body-text', textContent: text });
+  const infoCard = createElement({ tag: 'div', className: 'card-content__body-info' });
+  const userCard = createElement({ tag: 'div', className: 'card-content__body-info__user', textContent: formatUser(user) });
+  const dateCard = createElement({ tag: 'div', className: 'card-content__body-info__date', textContent: date ? date : formatDate() });
+  const arrowRight = createElement({ tag: 'div', className: 'card-content__arrow'});
+  const svgArrowRight = createIcon({ name: 'chevron-right', size: 18, className: 'card-content__arrow-icon', type: 'stroke' });
+  
+  arrowLeft.append(svgArrowLeft);
+  arrowRight.append(svgArrowRight);
+  infoCard.append(userCard, dateCard);
+  titleBlock.append(titleCard, basketDelete);
+  bodyCard.append(titleBlock, textCard, infoCard);
+  cardContent.append(arrowLeft, bodyCard, arrowRight);
+  card.append(cardContent);
+  
+  // Events
   const showModal = () => {
     const modalWin = createAddEditModal('Edit card', titleCard.textContent, textCard.textContent);
     card.append(modalWin);
     modalWin.showModal();
   }
-
   bodyCard.addEventListener('click', showModal);
 
-  const titleCard = createElement({ tag: 'p', className: 'card-content__body-title', textContent: title });
-  bodyCard.append(titleCard);
-  const textCard = createElement({ tag: 'div', className: 'card-content__body-text', textContent: text });
-  bodyCard.append(textCard);
-  const infoCard = createElement({ tag: 'div', className: 'card-content__body-info' });
-  const userCard = createElement({ tag: 'div', className: 'card-content__body-info__user', textContent: formatUser(user) });
-  if (!date) {date = formatDate()}
-  const dateCard = createElement({ tag: 'div', className: 'card-content__body-info__date', textContent: date });
-  infoCard.append(userCard);
-  infoCard.append(dateCard);
-  bodyCard.append(infoCard);
-  cardContent.append(bodyCard);
-
-  const arrowRight = createElement({ tag: 'div', className: 'card-content__arrow'});
-  const svgArrowRight = createIcon({ name: 'chevron-right', size: 18, className: 'card-content__arrow-icon', type: 'stroke' });
-  arrowRight.append(svgArrowRight);
-  cardContent.append(arrowRight);
-  
   return card;
 }
